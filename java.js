5e69c98b66a6bddfe4f6967f08bdc28c1e10e79e
Yomab <mabire.yoan@gmail.com>
2019-04-09 15:28:52 +0200

    exercice 4 fini

--- java.js
+++ java.js
@@ -1,3 +1,15 @@
-function passok(){
-  document.getElementById('id')
+
+function pass(){
+//creer une variable pour la valeur de l'id si ok
+var passverif =document.getElementById('password').value;
+//creer une variable pour la valeur de l'id de comparaison
+var passok =document.getElementById('confirmPassword').value;
+//si variable 1 et variable 2 sont identique alors affiche en vert
+if(passverif == passok) {
+document.getElementById('password').style.border= '2px solid green';
+//sinon affiche en rouge
+}else {
+document.getElementById('password').style.border='2px solid red';
+
+}
 }
